ec064a2b7299b776c67cda90cdc54a6e28e2edef
zh4r11 <azharimurii@gmail.com>
2024-12-15 01:38:46 +0700

    First Commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,73 @@
+<footer class="footer-area">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <!-- footer single wedget -->
+                <div class="col-md-6 col-lg-4">
+                    <!-- footer logo -->
+                    <div class="footer-logo">
+                        <a href="index.html"><img src="assets/images/logo/logo-furniture.jpg" alt="" /></a>
+                    </div>
+                    <!-- footer logo -->
+                    <div class="about-footer">
+                        <p class="text-info">We are a team of designers and developers that create high quality HTML template</p>
+                        <div class="need-help">
+                            <p class="phone-info">
+                                NEED HELP?
+                                <span>
+                                    (+800) 345 678 <br />
+                                    (+800) 123 456
+                                </span>
+                            </p>
+                        </div>
+                        <div class="social-info">
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="ion-social-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="ion-social-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="ion-social-youtube"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="ion-social-google"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="ion-social-instagram"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer single wedget -->
+                <div class="col-md-6 col-lg-2 mt-res-sx-30px mt-res-md-30px">
+                    <div class="single-wedge">
+                        <h4 class="footer-herading">Information</h4>
+                        <div class="footer-links">
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">My Account</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  Footer Bottom Area start -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <p class="copy-text">Copyright © <a href="#"> Cemilunch</a>. All Rights Reserved</p>
+                </div>
+                <div class="col-md-6 col-lg-8">
+                    {{-- <img class="payment-img" src="assets/images/icons/payment.png" alt="" /> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  Footer Bottom Area End-->
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,122 @@
+<header class="main-header home-2">
+    <!-- Header top Area Start  -->
+    <div class="header-top-nav">
+        <div class="container-fluid">
+            <div class="row">
+                <!--Left Start-->
+                <div class="col-lg-4 col-md-4">
+                    <div class="left-text">
+                        <p>Welcome you to Cemilunch store!</p>
+                    </div>
+                </div>
+                <!--Left end-->
+                <!--Right Start-->
+                <div class="col-lg-8 col-md-8 text-right">
+                    <div class="header-right-nav">
+                        <div class="dropdown-navs">
+                            <ul>
+                                <!-- Settings Start  -->
+                                <li class="dropdown after-n">
+                                    <a class="angle-icon" href="#">Settings</a>
+                                    <ul class="dropdown-nav">
+                                        <li><a href="my-account.html">My Account</a></li>
+                                        <li><a href="checkout.html">Checkout</a></li>
+                                        <li><a href="login.html">Login</a></li>
+                                    </ul>
+                                </li>
+                                <!-- Settings end  -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!--Right end-->
+            </div>
+        </div>
+    </div>
+    <!-- Header top Area end  -->
+    <!-- Header Navigation Area Start  -->
+    <div class="header-navigation sticky-nav">
+        <div class="container-fluid">
+            <div class="row">
+                <!--  Logo Area Start-->
+                <div class="col-md-2 col-sm-2">
+                    <div class="logo">
+                        <a href="index.html"><img src="assets/images/logo/logo-furniture.jpg" alt="" /></a>
+                    </div>
+                </div>
+                <!--  Logo Area end-->
+                <div class="col-md-10 col-sm-10">
+                    <!--Main Navigation Start -->
+                    <div class="main-navigation d-none d-lg-block">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <!--Main Navigation End -->
+                    <div class="header_account_area">
+                        <!--Cart info Start -->
+                        <div class="cart-info d-flex home-13">
+                            <div class="mini-cart-warp">
+                                <a href="#" class="count-cart"><span>$20.00</span></a>
+                                <div class="mini-cart-content">
+                                    <ul>
+                                        <li class="single-shopping-cart">
+                                            <div class="shopping-cart-img">
+                                                <a href="single-product.html"><img alt="" src="assets/images/product-image/mini-cart/1.jpg" /></a>
+                                                <span class="product-quantity">1x</span>
+                                            </div>
+                                            <div class="shopping-cart-title">
+                                                <h4><a href="single-product.html">Juicy Couture...</a></h4>
+                                                <span>$9.00</span>
+                                                <div class="shopping-cart-delete">
+                                                    <a href="#"><i class="ion-android-cancel"></i></a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="single-shopping-cart">
+                                            <div class="shopping-cart-img">
+                                                <a href="single-product.html"><img alt="" src="assets/images/product-image/mini-cart/2.jpg" /></a>
+                                                <span class="product-quantity">1x</span>
+                                            </div>
+                                            <div class="shopping-cart-title">
+                                                <h4><a href="single-product.html">Water and Wind...</a></h4>
+                                                <span>$11.00</span>
+                                                <div class="shopping-cart-delete">
+                                                    <a href="#"><i class="ion-android-cancel"></i></a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div class="shopping-cart-total">
+                                        <h4>Subtotal : <span>$20.00</span></h4>
+                                        <h4>Shipping : <span>$7.00</span></h4>
+                                        <h4>Taxes : <span>$0.00</span></h4>
+                                        <h4 class="shop-total">Total : <span>$27.00</span></h4>
+                                    </div>
+                                    <div class="shopping-cart-btn text-center">
+                                        <a class="default-btn" href="checkout.html">checkout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Cart info End -->
+                    </div>
+                </div>
+            </div>
+            <!-- mobile menu -->
+            <div class="mobile-menu-area">
+                <div class="mobile-menu">
+                    <nav id="mobile-menu-active">
+                        <ul class="menu-overflow">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!-- mobile menu end-->
+        </div>
+    </div>
+    <!--Header Bottom Account End -->
+</header>
